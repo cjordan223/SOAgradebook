635d2a9f9041f25f878b2c99859569a1ea1bd799
Erin Smajdek <esmajdek@csumb.edu>
2024-07-02 17:26:38 -0700

    Updates to Grade.java TODOs

--- src/main/java/com/cst438/domain/Grade.java
+++ src/main/java/com/cst438/domain/Grade.java
@@ -1,5 +1,6 @@
 package com.cst438.domain;
 
+import com.cst438.dto.AssignmentDTO;
 import jakarta.persistence.*;
 
 @Entity
@@ -8,10 +9,48 @@ public class Grade {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="grade_id")
     private int gradeId;
- 
-    // TODO complete this class
-    // add additional attribute for score
-    // add relationship between grade and assignment entities
-    // add relationship between grade and enrollment entities
-    // add getter/setter methods
+
+    @Column(name = "score")
+    int score;
+
+    @ManyToOne
+    @JoinColumn(name = "enrollment_id", referencedColumnName = "enrollment_id")
+    Enrollment enrollment;
+
+    @ManyToOne
+    @JoinColumn(name = "assignment_id", referencedColumnName = "assignment_id")
+    Assignment assignment;
+
+    public int getGradeId() {
+        return gradeId;
+    }
+
+    public void setGradeId(int gradeId) {
+        this.gradeId = gradeId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Enrollment getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(Enrollment enrollment) {
+        this.enrollment = enrollment;
+    }
+
+    public Assignment getAssignment() {
+        return assignment;
+    }
+
+    public void setAssignment(Assignment assignment) {
+        this.assignment = assignment;
+    }
+
 }
